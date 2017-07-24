8194028e427ae1ef9fda8ac0f4b3f24e3c7b556f
shfuyuyang <shduyuyang@163.com>
2017-07-25 00:27:33 +0800

    二叉树融合操作

--- binTree.c
+++ binTree.c
@@ -1,7 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+typedef struct binTree
+{
+    int val;
+    struct binTree * left;
+    struct binTree * right;
+}BinTree;
+
+BinTree * mergeTree(BinTree * t1,BinTree * t2)
+{
+    BinTree * returnT;
+    if(t1==NULL&&t2!=NULL)
+    {
+        return t2;
+    }
+    else if(t1!=NULL&&t2!=NULL)
+    {
+        return t1;
+    }
+    else if(t1==NULL&&t2==NULL)
+    {
+        return NULL;
+    }
+
+    returnT=malloc(sizeof(BinTree));
+    returnT->val=t1->val+t2->val;
+    returnT->left=mergeTree(t1->left,t2->left);
+    returnT->right=mergeTree(t1->right,t2->right);
+    return returnT;
+}
+
 void main()
 {
-    printf("test");
+
 }
