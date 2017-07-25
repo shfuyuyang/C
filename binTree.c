d52b4178c85e59d22efe35558cc04c1053efc96c
shfuyuyang <shduyuyang@163.com>
2017-07-26 01:24:30 +0800

    二叉树求最远的两个节点距离

--- binTree.c
+++ binTree.c
@@ -45,7 +45,56 @@ BinTree * invertTree(BinTree * root)
     return returnT;
 }
 
+int getBinaryTreeLongest(BinTree * root)
+{
+    int left=0,right=0;
+    if(root==NULL)
+    {
+        return 0;
+    }
+    left=getBinaryTreeLongest(root->left);
+    right=getBinaryTreeLongest(root->right);
+    if(left>=right)
+    {
+        return left+1;
+    }
+    else
+    {
+        return right+1;
+    }
+}
+//[1,2,3,4,5,6,7,8,9,0,null,null,null,null,null,1,null,null,null,0,0,null,1,null,1,null,1,1,null,null,null,1]
+int diameterOfBinaryTreeShadow(BinTree * root,int *max)
+{
+    int left=0,right=0;
+    if(root==NULL)
+    {
+        return 0;
+    }
+    left=getBinaryTreeLongest(root->left);
+    right=getBinaryTreeLongest(root->right);
+    if(*max<left+right)
+    {
+        *max=left+right;
+    }
+    diameterOfBinaryTreeShadow(root->left,max);
+    diameterOfBinaryTreeShadow(root->right,max);
+}
+
+int diameterOfBinaryTree(BinTree * root)
+{
+    int max=0;
+    diameterOfBinaryTreeShadow(root,&max);
+    return max;
+}
+
 void main()
 {
+    BinTree * root;
+    int a=0;
+    root=malloc(sizeof(BinTree));
+    root->val=1;
+    a=diameterOfBinaryTree(root);
+    printf("%d\n",a);
 
 }
